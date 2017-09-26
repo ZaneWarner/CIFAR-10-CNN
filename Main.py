80da34f06124985c6aca6ab3aa58a450a66515df
U-Zorc-PC\Zorc <zane.warner@gmail.com>
2017-09-26 10:51:48 -0700

    learning rate testing

--- Main.py
+++ Main.py
@@ -6,13 +6,13 @@
 ##########
 
 ##TODO
-# Set up training and validation
 # Tune hyperparams (batch size and learning rate)
 # Investigate & consider adding batch norm, dropout, learning rate decay
 # Also investigate going deeper - will likely involve some method of downsampling
 
 import numpy as np
 import tensorflow as tf
+import math
 import matplotlib.pyplot as plt
 
 ##### CIFAR-10 #####
@@ -35,16 +35,19 @@ imageDataDicts.append(unpickle('cifar-10-batches-py/data_batch_2'))
 imageDataDicts.append(unpickle('cifar-10-batches-py/data_batch_3'))
 imageDataDicts.append(unpickle('cifar-10-batches-py/data_batch_4'))
 
-numPrebuiltBatches = 4
-inputData = []
-outputLabels = []
-for i in range(numPrebuiltBatches):
-    inputData.append(imageDataDicts[i][b'data'])
-    outputLabels.append(imageDataDicts[i][b'labels'])
+inputData = imageDataDicts[0][b'data']
+outputLabels = np.array(imageDataDicts[0][b'labels'])
+for i in [1,2,3]:
+    np.append(inputData, imageDataDicts[i][b'data'], axis=0)
+    np.append(outputLabels, np.array(imageDataDicts[i][b'labels']), axis=0)
+    
+inputData = inputData.reshape(-1, 32, 32, 3)
 
 validationDataDict = unpickle('cifar-10-batches-py/data_batch_5')
 validationInputData = validationDataDict[b'data']
-validationOutputLabels = validationDataDict[b'labels']
+validationOutputLabels = np.array(validationDataDict[b'labels'])
+
+validationInputData = validationInputData.reshape(-1, 32, 32, 3)
 
 ##### Graph Creation #####
 x = tf.placeholder(tf.float32, [None, 32, 32, 3])
@@ -65,16 +68,69 @@ def makeTwoConvLayersGraph(x):
     a1 = tf.nn.relu(c1, name="a1")
     c2 = tf.nn.conv2d(a1, filter2, strides=[1,1,1,1], padding="SAME", name="c2") + bias2
     a2 = tf.nn.relu(c2, name="a2")
-    fc3 = tf.layers.dense(a2, units=10, name="fc3") #note that this name will be made weird by the autoaddition of a bias node
+    a2Flat = tf.reshape(a2, [-1, 32*32*16])
+    fc3 = tf.layers.dense(a2Flat, units=10, name="fc3") #note that this name will be made weird by the autoaddition of a bias node
     return fc3
 
 outputLayer = makeTwoConvLayersGraph(x)
 loss = tf.losses.softmax_cross_entropy(tf.one_hot(y, 10), outputLayer)
 
 optimizer = tf.train.AdamOptimizer(1e-5)
-train_step = optimizer.minimize(loss)
+trainer = optimizer.minimize(loss)
 
 netSaver = tf.train.Saver()
 
-##### Training #####
+##### Running the Network #####
+def runNN(session, xIn, yIn, trainer=None, epochs=1, batchSize=100, printEvery = 20, lossPlot=False, plotname='lossplot.png'):
+    lossValues = []
+    numExamples = len(xIn)
+    numBatches = math.ceil(numExamples/batchSize)
+    batchRemainder = numExamples % batchSize
+    batchIndices = np.arange(numExamples)
+    for epoch in range(epochs):
+        np.random.shuffle(batchIndices)
+        batchLossAggregator = 0
+        for batch in range(numBatches):
+            batchLower = batch*batchSize
+            if batch != numBatches:
+                batchUpper = batchLower+batchSize
+            else:
+                batchUpper = batchLower+batchRemainder
+            thisBatch = batchIndices[batchLower:batchUpper]
+            values = {x : xIn[np.array(thisBatch)],
+                      y : yIn[np.array(thisBatch)]}
+            if trainer is not None:
+                batchLossValue, _ = session.run([loss, trainer], feed_dict = values)
+            else:
+                batchLossValue = session.run(loss, feed_dict = values)
+            batchLossAggregator += batchLossValue
+        lossValues.append(batchLossAggregator)
 
+        if epoch % printEvery == 0 and trainer is not None:
+            print('Epoch {}--Loss Value: {}'.format(epoch+1, lossValues[epoch]))
+    
+    netSaver.save(session, './NetworkSaves/CNN')   
+    if lossPlot == True:
+        plt.plot(lossValues)
+        plt.savefig(plotname)
+        plt.clf()
+    
+    return lossValues
+
+#### Execution #####
+# with tf.Session() as sess:
+#     with tf.device("/cpu:0"):
+#         sess.run(tf.global_variables_initializer())
+#         print('Training')
+#         runNN(sess, inputData[:100,:], outputLabels[:100,], trainer=trainer, batchSize=50, epochs=2, printEvery=2, lossPlot=True)
+        #print('Validation')
+        #runNN(sess, validationInputData, validationOutputLabels, trainer=None, batchSize=10, lossPlot=True)
+        
+for lrTest in range(2, 10, 1):
+    lrUse = 10^(-lrTest)
+    optimizer = tf.train.AdamOptimizer(lrUse)
+    with tf.Session() as sess:
+        with tf.device("/gpu:0"):
+            sess.run(tf.global_variables_initializer())
+            runNN(sess, inputData[:2000,:], outputLabels[:2000,], trainer=trainer, batchSize=100, epochs=30, printEvery=5, lossPlot=True, plotname='lr{}'.format(lrTest))
+                
