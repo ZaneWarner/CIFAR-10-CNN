8361b339d72685ccaeef1477baed103e942363a4
U-Zorc-PC\Zorc <zane.warner@gmail.com>
2017-10-09 13:50:56 -0700

    fixed filter size, added random cropping and epoch-wise augmentation

--- DataAugmenter.py
+++ DataAugmenter.py
@@ -8,22 +8,35 @@
 ##### Data Augmenter #####
 # this module contains functions that are used to augment the training data to combat overfitting/increase mdoel generalization
 # they are designed to be called in the main module
-# input images are assumed to be numpy arrays with dimensions 32x32x3, which is width x height x channels
+# images are assumed to be square with 3 color channels
 ##########
 
 import numpy as np
 
 def HorizontalFlip(img):
-    flippedImg = np.zeros([32, 32, 3])
-    for i in range(32):
-        flippedImg[:,i,:] = img[:,31-i,:]
+    flippedImg = np.zeros(img.shape)
+    imgWidth = len(img[0,:,0])
+    for i in range(imgWidth):
+        flippedImg[:,i,:] = img[:,imgWidth-(i+1),:]
     return flippedImg
 
+def RandomHorizontalFlip(img, flipProbability=.5):
+    probabilitySample = np.random.random_sample()
+    if probabilitySample < flipProbability:
+        img = HorizontalFlip(img)
+    return(img)
+
 def RandomHueShift(img, hueFloor=.95, hueCeil=1.05):
-    shiftedImg = np.zeros([32, 32, 3])
+    shiftedImg = np.zeros(img.shape)
     shiftAmts = np.random.uniform(hueFloor, hueCeil, 3)
     for i in range(3):
         shiftedImg[:,:,i] = img[:,:,i]*shiftAmts[i]
     return shiftedImg
         
-#def RandomCropper(img, ):
+def RandomCropper(img, cropPixels=4):
+    imgSize = len(img[:,0,0])
+    topBuffer = np.random.randint(cropPixels+1) #+1 because randint uses a half-open interval (upper half open)
+    leftBuffer = np.random.randint(cropPixels+1)
+    croppedImgSize = imgSize-cropPixels
+    croppedImg = img[topBuffer:(croppedImgSize+topBuffer), leftBuffer:(croppedImgSize+leftBuffer), :]
+    return croppedImg
--- Main.py
+++ Main.py
@@ -24,7 +24,7 @@
 import numpy as np
 import tensorflow as tf
 import math
-from DataAugmenter import HorizontalFlip, RandomHueShift
+from DataAugmenter import HorizontalFlip, RandomHueShift, RandomHorizontalFlip, RandomCropper
 import matplotlib.pyplot as plt
 
 ##### CIFAR-10 #####
@@ -53,6 +53,7 @@ for i in [1,2,3]:
     np.append(inputData, imageDataDicts[i][b'data'], axis=0)
     np.append(outputLabels, np.array(imageDataDicts[i][b'labels']), axis=0)
     
+# Adjust Channel Means To 0 
 inputData = np.float64(inputData.reshape(-1, 32, 32, 3))
 channel1Mean = np.mean(inputData[:,:,:,0])
 channel2Mean = np.mean(inputData[:,:,:,1])
@@ -61,25 +62,19 @@ inputData[:,:,:,0] -= channel1Mean
 inputData[:,:,:,1] -= channel2Mean
 inputData[:,:,:,2] -= channel3Mean
  
-augmentedData = np.zeros(inputData.shape)
-for i in range(len(inputData[:,0,0,0])):
-    img = inputData[i,:,:,:]
-    newImg = RandomHueShift(HorizontalFlip(img))
-    augmentedData[i,:,:,:] = newImg
-inputData = np.append(inputData, augmentedData, 0)
-outputLabels = np.append(outputLabels, outputLabels, 0)
- 
+#Load Validation
 validationDataDict = unpickle('cifar-10-batches-py/data_batch_5')
 validationInputData = validationDataDict[b'data']
 validationOutputLabels = np.array(validationDataDict[b'labels'])
- 
+
+# Adjust Validation Channel Means To 0 
 validationInputData = np.float64(validationInputData.reshape(-1, 32, 32, 3))
 validationInputData[:,:,:,0] -= channel1Mean
 validationInputData[:,:,:,1] -= channel2Mean
 validationInputData[:,:,:,2] -= channel3Mean
 
 ##### Graph Creation #####
-x = tf.placeholder(tf.float32, [None, 32, 32, 3])
+x = tf.placeholder(tf.float32, [None, 28, 28, 3]) #The 28s depend on the amount of random cropping used. In particular, they should be 32 minus the amount cropped.
 y = tf.placeholder(tf.int64, [None])
 trainingMode = tf.placeholder(tf.bool) #True indicates that it is training, false is test time
 
@@ -87,36 +82,42 @@ def makeTwoConvLayersGraph(x, dropoutProb=.5, betaConv=100, betaFC=.1):
     #This creates the graph for a network that goes conv-relu-conv-relu-affine
     #The output is not activated after the dense later since it is designed to be fed to a loss function (e.g. a softmax)
     #Initialize variables
-    filter1 = tf.get_variable("filter1", [32,32,3,16])
+    filter1 = tf.get_variable("filter1", [2,2,3,16])
     bias1 = tf.get_variable("bias1", [16])
-    filter2 = tf.get_variable("filter2", [16,16,16,16])
+    filter2 = tf.get_variable("filter2", [2,2,16,16])
     bias2 = tf.get_variable("bias2", [16])
-    filter3 = tf.get_variable("filter3", [16,16,16,8])
+    filter3 = tf.get_variable("filter3", [2,2,16,8])
     bias3 = tf.get_variable("bias3", [8])
-    fcWeights4 = tf.get_variable("fcWeights4", [512, 1000])
+    fcWeights4 = tf.get_variable("fcWeights4", [7*7*8, 1000]) #these dimensions depend on the amount of random cropping
     fcBias4 = tf.get_variable("fcBias4", [1000])
     
     #Build Graph
+    # conv-relu-bn-dropout-maxpool 1
     c1 = tf.nn.conv2d(x, filter1, strides=[1,1,1,1], padding="SAME", name="c1") + bias1
     a1 = tf.nn.relu(c1, name="a1")
     bn1 = tf.layers.batch_normalization(a1, axis=3, training=trainingMode, name="bn1")
     drpo1 = tf.layers.dropout(bn1, rate=dropoutProb, name="drpo1")
     mp1 = tf.nn.max_pool(drpo1, ksize=[1,2,2,1], strides=[1,2,2,1], padding="SAME", name='mp1')
+    # conv-relu-bn-dropout-maxpool 2
     c2 = tf.nn.conv2d(mp1, filter2, strides=[1,1,1,1], padding="SAME", name="c2") + bias2
     a2 = tf.nn.relu(c2, name="a2")
     bn2 = tf.layers.batch_normalization(a2, axis=3, training=trainingMode, name="bn2")
     drpo2 = tf.layers.dropout(bn2, rate=dropoutProb, name="drpo2")
     mp2 = tf.nn.max_pool(drpo2, ksize=[1,2,2,1], strides=[1,2,2,1], padding="SAME", name='mp2')
+    # conv-relu-bn
     c3 = tf.nn.conv2d(mp2, filter3, strides=[1,1,1,1], padding="SAME", name="c3") + bias3
     a3 = tf.nn.relu(c3, name="a3")
     bn3 = tf.layers.batch_normalization(a3, axis=3, training=trainingMode, name="bn3")
-    bn3Flat = tf.reshape(bn3, [-1, 8*8*8])
-    fc4 = tf.matmul(bn3Flat, fcWeights4) + fcBias4
+    bn3Flat = tf.reshape(bn3, [-1, 7*7*8]) #these dimensions depend on the amount of random cropping
+    # fc-relu-bn-dropout
+    fc4 = tf.matmul(bn3Flat, fcWeights4) + fcBias4 #A 1000 hidden unit layer, done manually to make regularization more straightforward
     a4 = tf.nn.relu(fc4, name="a4")
     bn4 = tf.layers.batch_normalization(a4, training=trainingMode, name="bn4")
     drpo4 = tf.layers.dropout(bn4, rate=dropoutProb, name="drpo4")
+    # fc output
     fc5 = tf.layers.dense(drpo4, 10, name="fc5")
     
+    #regularization
     convRegularizer = betaConv*(tf.nn.l2_loss(filter1, name="filter1Reg") + tf.nn.l2_loss(filter2, name="filter2Reg") + tf.nn.l2_loss(filter3, name="filter3Reg"))
     fcRegularizer =  betaFC*tf.nn.l2_loss(fcWeights4, name="fcWeights4Reg")
     l2Regularizer = convRegularizer + fcRegularizer
@@ -128,7 +129,7 @@ numCorrect = tf.reduce_sum(tf.cast(tf.equal(tf.argmax(outputLayer, axis=1), y), 
 
 update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 
-optimizer = tf.train.AdamOptimizer(1e-5)
+optimizer = tf.train.AdamOptimizer(5e-5)
 with tf.control_dependencies(update_ops):
     trainer = optimizer.minimize(loss)
 
@@ -143,6 +144,14 @@ def runNN(session, xIn, yIn, trainer=None, epochs=1, batchSize=100, printEvery =
     batchRemainder = numExamples % batchSize
     batchIndices = np.arange(numExamples)
     for epoch in range(epochs):
+        #data augmentation
+        xAugmented = np.zeros([numExamples, 28, 28, 3]) #The 28s are yet another place that the amount of cropping is hardcoded.
+        if trainer is not None:
+            for i in range(numExamples):
+                xAugmented[i, :, :, :] = RandomHueShift(RandomHorizontalFlip(RandomCropper(xIn[i, :, :, :], cropPixels=4)))
+        else:
+            for i in range(numExamples):
+                xAugmented[i, :, :, :] = RandomCropper(xIn[i, :, :, :], cropPixels=4) #stochasticity during test time??????? this seems bad but idk how else to make the images fit
         np.random.shuffle(batchIndices)
         batchLossAggregator = 0
         correctAggregator = 0
@@ -154,7 +163,7 @@ def runNN(session, xIn, yIn, trainer=None, epochs=1, batchSize=100, printEvery =
                 batchUpper = batchLower+batchRemainder
             thisBatch = batchIndices[batchLower:batchUpper]
             if trainer is not None:
-                values = {x : xIn[np.array(thisBatch)],
+                values = {x : xAugmented[np.array(thisBatch)],
                           y : yIn[np.array(thisBatch)],
                           trainingMode : True}
                 batchLossValue, correct, _ = session.run([loss, numCorrect, trainer], feed_dict = values)
@@ -181,10 +190,10 @@ def runNN(session, xIn, yIn, trainer=None, epochs=1, batchSize=100, printEvery =
 
 #### Execution #####
 with tf.Session() as sess:
-    with tf.device("/gpu:0"):
+    with tf.device("/cpu:0"):
         sess.run(tf.global_variables_initializer())
         print('Training')
-        runNN(sess, inputData, outputLabels, trainer=trainer, epochs=65, printEvery=5, lossPlot=False)
+        runNN(sess, inputData, outputLabels, trainer=trainer, epochs=81, printEvery=10, lossPlot=False)
         print('Validation')
         runNN(sess, validationInputData, validationOutputLabels, trainer=None, batchSize=1000, lossPlot=False)
 
